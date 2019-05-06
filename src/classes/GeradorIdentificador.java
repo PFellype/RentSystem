eae123a99af5c29dffe015f553e789a37c841a34
Pablo Fellype <pablopslv@gmail.com>
2019-05-06 10:28:52 -0300

    Iniciando o Projeto

---
+++ src/classes/GeradorIdentificador.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
+
+/**
+ *
+ * @author Pablo Fellype
+ */
+public class GeradorIdentificador {
+    private int identificador = 0;
+    String nomeDoArquivo = "C:\\Users\\Pablo Fellype\\Documents\\RentSystem\\DadosID.txt";
+    
+    public GeradorIdentificador() throws FileNotFoundException, IOException{
+        FileReader fr = new FileReader(nomeDoArquivo);
+        BufferedReader br = new BufferedReader(fr);
+        String linha = br.readLine();
+        String vetorString[] = linha.split(";");
+        identificador = Integer.parseInt(vetorString[0]);
+        br.close();
+    }
+
+    public int getIdentificador() {
+        return +identificador;
+    }
+    
+    public void finalize() throws IOException{
+        //cria arquivo
+        FileWriter fw = new FileWriter(nomeDoArquivo, false);
+        //cria buffer do arquivo
+        BufferedWriter bw = new BufferedWriter(fw);
+        //escreve no arquivo
+        String saida = identificador + "\n";
+        bw.write(saida);
+        //fecha o arquivo
+        bw.close();
+    }
+    
+}
---
+++ src/classes/Marca.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Pablo Fellype
+ */
+public class Marca {
+    //Atributos
+    private int ID = 0;
+    private String marca = "";
+    
+    //Métodos
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+}
